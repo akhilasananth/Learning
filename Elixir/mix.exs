1ac414006c1873f6296a75c206c2a755d5db634c
Akhila Ananth <akhilaananth@Akhilas-iMac.fios-router.home>
2022-05-17 21:24:15 -0400

    simple Elixir server

---
+++ Elixir/mix.exs
@@ -0,0 +1,32 @@
+defmodule SimpleServer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :simple_server,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :sqlite_ecto, :ecto, :plug_cowboy, :plug, :poison],
+      mod: {SimpleServer.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:plug_cowboy, "~> 1.0.0"},
+      {:plug, "~> 1.5"},
+      {:poison, "~> 3.1", override: true},
+      {:sqlite_ecto, "~> 1.0.0", override: true}
+      {:ecto, "~> 1.0"}
+    ]
+  end
+end
